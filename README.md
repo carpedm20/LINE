73e7f50d016ecac142271119d8ab102d94ee59f4
carpedm20 <carpedm20@gmail.com>
2014-08-02 08:38:40 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+
+                                 //  LINE  //
+
+                           May the LINE be with you!
+
+                       The documentation is available at
+                        http://packages.python.org/line
+
+                                by Taehoon Kim
+                                  (carpedm20)
---
+++ setup.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Line -- May the LINE be with you...
+=========================================
+
+If you're reading this code, you should know what LINE is.
+
+Just play. Have fun. Enjoy the LINE!
+```````````````````````````
+
+::
+
+    >>> from line import Client
+
+Links
+`````
+
+* `GitHub repository <http://github.com/carpedm20/line>`_
+* `development version
+  <http://github.com/carpedm20/line/zipball/master>`_
+
+
+"""
+from __future__ import with_statement
+import re
+
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
+
+# detect the current version
+with open('line/__init__.py') as f:
+    version = re.search(r'__version__\s*=\s*\'(.+?)\'', f.read()).group(1)
+assert version
+
+setup(
+    name='line',
+    version=version,
+    license='BSD',
+    author='Taehoon Kim',
+    author_email='carpedm20@gmail.com',
+    url='http://pythonhosted.org/line',
+    description='May the LINE be with you...',
+    long_description=__doc__,
+    platforms='any',
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 0 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Communications :: Chat',
+    ],
+    install_requires=['setuptools'],
+)
