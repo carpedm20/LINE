485a24fe1682b5febbba6ee9bd7dc1fbcbd85024
Taehoon Kim <carpedm20@gmail.com>
2014-08-25 14:40:28 +0900

    README.rst encoding error fix

--- setup.py
+++ setup.py
@@ -36,40 +36,42 @@ with open('line/__init__.py') as f:
     version = re.search(r'__version__\s*=\s*\'(.+?)\'', f.read()).group(1)
 assert version
 
-setup(
-    name='line',
-    packages=['line'],
-    version=version,
-    description='May the LINE be with you...',
-    long_description=open('README.rst').read(),
-    license='BSD License',
-    author='Taehoon Kim',
-    author_email='carpedm20@gmail.com',
-    url='http://carpedm20.github.io/line',
-    keywords=['line'],
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.1',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Communications :: Chat',
-    ],
-    install_requires=[
-        'requests',
-        'curve',
-        'rsa'
-    ],
-)
+import codecs
+with codecs.open('README.rst','r',encoding='utf8') as f:
+    setup(
+        name='line',
+        packages=['line'],
+        version=version,
+        description='May the LINE be with you...',
+        long_description=f.read(),
+        license='BSD License',
+        author='Taehoon Kim',
+        author_email='carpedm20@gmail.com',
+        url='http://carpedm20.github.io/line',
+        keywords=['line'],
+        classifiers=[
+            'Development Status :: 2 - Pre-Alpha',
+            'Environment :: Console',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: BSD License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.1',
+            'Programming Language :: Python :: 3.2',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: Implementation :: CPython',
+            'Programming Language :: Python :: Implementation :: PyPy',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+            'Topic :: Communications :: Chat',
+        ],
+        install_requires=[
+            'requests',
+            'curve',
+            'rsa'
+        ],
+    )
